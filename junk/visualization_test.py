f7d57d1421000b2e8a79a9dff179b8fe7c8d3fc0
Robert Loftin <rloftin4400@gmail.com>
2022-02-28 20:08:28 +0100

    added example implementation of visualization for coordination game

--- junk/visualization_test.py
+++ junk/visualization_test.py
@@ -1,8 +1,9 @@
 """Test of Pyglet visualization for Coordination game"""
+from re import L
 from gym.spaces import Discrete, Box
 import numpy as np
 import pyglet
-import time
+from pyglet.gl import *
 
 
 class CoordinationGame:
@@ -22,7 +23,10 @@ class CoordinationGame:
             self.action_space[pid] = Discrete(self._num_actions)
         
         self._current_stage = 0
-        self._prev_actions = None
+        self._prev = []
+
+        self._num_episodes = 0
+        self._last_obs = None
 
     def _obs(self, actions=None):
         obs = {}
@@ -42,6 +46,8 @@ class CoordinationGame:
 
     def reset(self):
         self._current_stage = 0
+        self._prev = []
+
         return self._obs()
 
     def step(self, actions):
@@ -62,77 +68,20 @@ class CoordinationGame:
         done = self._num_stages <= self._current_stage
         dones = {pid:done for pid in range(self._num_players)}
 
+        # Save previous actions for rendering
+        prev_actions = [actions[pid] for pid in range(self._num_players)]
+        self._prev.append((prev_actions, reward))
+
         return obs, rewards, dones, None
-
-    def _open_window(self):
-        # Open window
-        width = self._cell_size * self._num_stages
-        height = self._cell_size
-        config = pyglet.gl.Config(sample_buffers=1, samples=4)
-        self._window = pyglet.window.Window(width, height, 
-                            config=config, caption="Coordination Game")
-
-        @self._window.event
-        def on_close():
-            self._window.close()
-            self._window = None  # Allow window to be garbage collected
-            self._window_closed = True
-
-        # Initialize text overlay
-        self._labels = []
-        x_pos = self._cell_size // 2
-        y_pos = self._cell_size // 10
-
-        for stage in range(self._num_stages):
-            self._labels.append(pyglet.text.Label(f"stage {stage + 1}",
-                          font_name="Arial",
-                          font_size=36,
-                          x=x_pos, y=y_pos,
-                          anchor_x="center", anchor_y="center",
-                          color=(1,1,1,1), bold=True))
-            x_pos += self._cell_size
-
-    def _draw(self):
-        
-        # Set window as current OpenGL context
-        self._window.switch_to()
-
-        # Clear window
-        pyglet.gl.glClearColor(0, 0, 0, 1)
-        self._window.clear()
-
-        # Draw text overlay
-        for label in self._labels:
-            print("label draw")
-            label.draw()
-
-    def render(self):  # TODO: Drop the "render" method altogether and let the environment implement its own loop
-
-        # Open window if needed
-        if self._window is None and not self._window_closed:
-            self._open_window()
-
-        # Handle window events - allow window to close
-        if not self._window_closed:
-            self._window.dispatch_events()
-
-        # Render frame to window - need to check that window hasn't closed
-        if not self._window_closed:
-            self._draw()
-
-    @property
-    def window_closed(self):
-        return self._window_closed
 
     def visualize(self, 
                   policies={}, 
                   max_episodes=None, 
                   max_steps=None,
-                  step_interval=2.0,
+                  step_interval=1.5,
                   cell_size=200,
                   max_width=1600):
-        
-        # NOTE: Pyglet seems a bit flaky, does it have anything to do with WSL?
+        # NOTE: There is a bug in VcXsrv with WSL that causes problems if we move the window
 
         # Enforce maximum window width
         if cell_size * self._num_stages > max_width:
@@ -145,8 +94,6 @@ class CoordinationGame:
         window = pyglet.window.Window(width, height, 
                     config=config, caption="Coordination Game")
 
-        # Initialize geometry
-
         # Initialize text overlay
         labels = []
         x_pos = cell_size // 2
@@ -155,13 +102,19 @@ class CoordinationGame:
         for stage in range(self._num_stages):
             labels.append(pyglet.text.Label(f"stage {stage + 1}",
                           font_name="Arial",
-                          font_size=16,
+                          font_size=14,
                           x=x_pos, y=y_pos,
                           anchor_x="center", anchor_y="center",
                           color=(255,255,255,255), bold=True))
             x_pos += cell_size
 
-        # TODO: Schedule environment updates
+        # Initialize geometry
+        offset = cell_size // 5
+        row_size = (cell_size - 2*offset) // self._num_actions
+        column_size = (cell_size - 2*offset) // self._num_players
+
+        fail_icon = pyglet.shapes.Rectangle(0, 0, column_size, row_size, color=(255,0,0))
+        good_icon = pyglet.shapes.Rectangle(0, 0, column_size, row_size, color=(0,255,0))
 
         # Add drawing function
         @window.event
@@ -171,11 +124,63 @@ class CoordinationGame:
             window.clear()
             
             # Render geometry
+            glLoadIdentity()
 
+            for stage, (actions, reward) in enumerate(self._prev):
+                if 0 == reward:
+                    icon = fail_icon
+                else:
+                    icon = good_icon
+                
+                glPushMatrix()
+                glTranslatef(offset + stage * cell_size, offset, 0)
+
+                for player, action in enumerate(actions):
+                    glPushMatrix()
+                    glTranslatef(column_size * player, row_size * action, 0)
+                    icon.draw()
+                    glPopMatrix()
+
+                glPopMatrix()
 
             # Draw text overlay
             for label in labels:
                 label.draw()
+
+        # Initialize agents
+        agents = {}
+        for pid in self.action_space.keys():
+            if pid in policies:
+                agents[pid] = policies[pid].make_agent()
+
+        # Schedule environment updates
+        if max_steps is None:
+            max_steps = self._num_stages
+        else:
+            max_steps = min(self._num_stages, max_steps)
+
+        def update(dt):
+            if self._current_stage >= max_steps:
+                self._num_episodes += 1
+
+                if max_episodes is not None and self._num_episodes >= max_episodes:
+                    pyglet.app.exit()
+
+                self._last_obs = self.reset()
+            else:
+                actions = {}
+                for pid, space in self.action_space.items():
+                    if pid in agents:
+                        actions[pid] = agents[pid].act(self._last_obs)
+                    else:
+                        actions[pid] = space.sample()
+                
+                self._last_obs, _, _, _ = self.step(actions)
+
+        self._num_episodes = 0
+        self._last_obs = self.reset()
+
+        pyglet.clock.schedule_interval(update, step_interval)
 
         # Launch game loop - returns when window is closed or max-episodes reached
         pyglet.app.run()
@@ -195,32 +200,4 @@ if __name__ == "__main__":
     
 
     # Launch visualization
-    env.visualize()  # NOTE: Is it possible that there is some environment variable we are missing here?
-
-    # Run episodes
-    '''
-    sleep = 1.0 / FPS
-
-    for episode in range(NUM_EPISODES):
-        print(f"Episode {episode}")
-
-        obs = env.reset()
-        dones = {"__all__": False}
-
-        env.render()
-        time.sleep(sleep)
-
-        while not all(dones.values()):
-            actions = {}
-            for id, space in env.action_space.items():
-                actions[id] = space.sample()
-            
-            obs, reward, dones, _ = env.step(actions)
-
-            env.render()
-
-            if env.window_closed:
-                exit()
-            
-            time.sleep(sleep)
-    '''
+    env.visualize()
